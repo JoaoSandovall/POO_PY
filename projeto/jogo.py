5dff444ca842c5eb3f882945fbd321aca0efb046
joaoSandovall <jpsandovalveras@gmail.com>
2025-09-04 08:44:48 -0300

    Sixth commit

--- projeto/jogo.py
+++ projeto/jogo.py
@@ -1,3 +1,5 @@
+import random
+
 # personagem: Classe mae
 # Heroi: controlado pelo usuario
 # inimigo: adversario do usuario
@@ -26,7 +28,7 @@ class personagem:
             self.__vida = 0
             
     def atacar(self, alvo):
-        dano = self.__nivel * 2
+        dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4)
         alvo.receber_ataque(dano)
         print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
         
@@ -41,6 +43,10 @@ class Heroi(personagem):
     def exibir_detalhes(self):
         return f"{super().exibir_detalhes()}\nHabilidade: {self.get_habilidade()}\n"
             
+    def ataque_especial(self, alvo):
+        dano = random.randint(self.get_nivel() * 5, self.get_nivel() * 8)
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
         
 class Inimigo(personagem):
     def __init__(self, nome, vida, nivel, tipo):
@@ -58,8 +64,8 @@ class Jogo:
     """ Classe de orquestradora do jogo"""
     
     def __init__(self) -> None:
-        self.heroi = Heroi(nome="Herói", vida=100, nivel=3, habilidade="Espada")
-        self.inimigo = Inimigo(nome="morcego", vida=50, nivel=1, tipo="voador")
+        self.heroi = Heroi(nome="Herói", vida=80, nivel=3, habilidade="Espada")
+        self.inimigo = Inimigo(nome="morcego", vida=50, nivel=2, tipo="voador")
         
     def iniciar_batalha(self):
         """ Fazer a gestão da batalha em turnos """
@@ -77,8 +83,13 @@ class Jogo:
             
             if escolha == '1':
                 self.heroi.atacar(self.inimigo)
+            elif escolha == '2':
+                self.heroi.ataque_especial(self.inimigo)
             else:
                 print("Escolha inválida. escolha novamente.")
+            
+            if self.inimigo.get_vida() > 0:
+                self.inimigo.atacar(self.heroi)
         
         if self.heroi.get_vida() > 0:
             print("\nParabéns! você venceu a batalha!")
